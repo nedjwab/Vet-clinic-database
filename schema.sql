53a91e04788cbdc6a2a913034723cab617454725
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-07-19 21:11:27 +0100

    Add FOREIGN keys

--- schema.sql
+++ schema.sql
@@ -24,3 +24,9 @@ CREATE TABLE species (
 );
 
 ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
