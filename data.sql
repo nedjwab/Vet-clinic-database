88afb0390b65f8e643b65eaa4c4a35aa859a8feb
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-07-20 00:44:22 +0100

    Add owner_id to the animals table

--- data.sql
+++ data.sql
@@ -28,3 +28,18 @@ INSERT INTO species (name) VALUES
 UPDATE animals SET species_id=1;
 UPDATE animals SET species_id=2
 WHERE name like '%mon';
+
+UPDATE animals SET owner_id=1
+WHERE name='Agumon';
+
+UPDATE animals SET owner_id=2
+WHERE name='Gabumon' OR name='Pikachu';
+
+UPDATE animals SET owner_id=3
+WHERE name='Devimon' OR name='Plantmon';
+
+UPDATE animals SET owner_id=4
+WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
+
+UPDATE animals SET owner_id=5
+WHERE name='Angemon' OR name='Boarmon';
