0cb3db796563c9da779aabca8b686555469d9dc5
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-07-25 18:06:00 +0100

    Analyze the two other queries

--- queries.sql
+++ queries.sql
@@ -222,4 +222,6 @@ ON animals.species_id=species.id
 WHERE vets.name='Maisy Smith'
 GROUP BY species.name;
 
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
