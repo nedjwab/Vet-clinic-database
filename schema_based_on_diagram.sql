aa191cc42a6f2de00bb2c451c194240b378f35e5
ilham bouaik <ibouaik@gmail.com>
2022-07-26 10:51:49 +0100

    create table medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,11 @@ CREATE TABLE patients (
     name varchar(100),
     date_of_birth DATE
 );
+
+CREATE TABLE medical_hitories (
+    id SERIAL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patients_id INT,
+    FOREIGN KEY (patients_id) REFERENCES patients (id),
+    status varchar(100)
+);
